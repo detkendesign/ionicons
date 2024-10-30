0cde032b168be1c355752888049fb9c663d97dde
Anton Detken <69902586+ant0n7@users.noreply.github.com>
2024-10-30 11:19:14 +0100

    Set react version to `detect`.
    This resolves "Warning: React version not specified in eslint-plugin-react settings. See https://github.com/jsx-eslint/eslint-plugin-react#configuration".

--- eslint.config.js
+++ eslint.config.js
@@ -10,6 +10,11 @@ export default [
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
     rules: {
       "react/react-in-jsx-scope": "off",
     },

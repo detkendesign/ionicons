70453f4119771b6d0567b7f3a777939494d03d3c
Anton Detken <69902586+ant0n7@users.noreply.github.com>
2024-10-30 11:18:06 +0100

    Disable rule react/react-in-jsx-scope as we use React 17+.

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable react/react-in-jsx-scope */
-import { type FC } from "react";
+import type { FC } from "react";
 
 export const Icon: FC = () => {
   return <span role="img">icon</span>;

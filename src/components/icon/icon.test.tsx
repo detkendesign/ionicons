48bb438b87b7198b71d78892b8030cbb9814d44d
rehkmansa <godspowernathaniel25@gmail.com>
2024-11-02 11:20:08 +0100

    Add tests for icon size.

--- src/components/icon/icon.test.tsx
+++ src/components/icon/icon.test.tsx
@@ -1,8 +1,15 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import { Icon } from "./icon.js";
+import { ICON_SIZE } from "./constants.js";
 import { describe, it, expect } from "vitest";
 import React from "react";
+
+const checkSizeDimension = (icon: SVGSVGElement, size: number) => {
+  expect(icon).toBeInTheDocument();
+  expect(icon).toHaveAttribute("width", size.toString());
+  expect(icon).toHaveAttribute("height", size.toString());
+};
 
 describe("Icon Component", () => {
   const validIcon = "accessibility";
@@ -28,5 +35,19 @@ describe("Icon Component", () => {
     render(<Icon name={validIcon} data-testid="custom-icon" />);
     const icon = screen.getByTestId("custom-icon");
     expect(icon).toBeInTheDocument();
+  });
+
+  it("renders the correct size when using fixed sizes", () => {
+    const size = "small";
+    const { container } = render(<Icon name={validIcon} size={size} />);
+    const icon = container.querySelector("svg")!;
+    checkSizeDimension(icon, ICON_SIZE[size]);
+  });
+
+  it("renders the correct size when passed a number", () => {
+    const size = 24;
+    const { container } = render(<Icon name={validIcon} size={size} />);
+    const icon = container.querySelector("svg")!;
+    checkSizeDimension(icon, size);
   });
 });
